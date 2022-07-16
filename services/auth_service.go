edc02d4ef6ec7c91bfebf8d51b047d8fdaee438d
= <suttapak.matee@gmail.com>
2022-07-16 13:35:15 +0700

    Feat : Add new Save page to database.

--- services/auth_service.go
+++ services/auth_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+	"strconv"
 
 	fb "github.com/huandu/facebook/v2"
 	"github.com/suttapak/cacf/dto"
@@ -14,11 +15,12 @@ import (
 type authService struct {
 	shopRepo     repositories.ShopRespository
 	facebookRepo repositories.FacebookRespository
+	pageRepo     repositories.PageFacebookRepository
 }
 
 func NewAuthService(shopRepo repositories.ShopRespository,
-	facebookRepo repositories.FacebookRespository) AuthService {
-	return &authService{shopRepo, facebookRepo}
+	facebookRepo repositories.FacebookRespository, pageRepo repositories.PageFacebookRepository) AuthService {
+	return &authService{shopRepo, facebookRepo, pageRepo}
 }
 
 func (s authService) SignIn(shop dto.SignInDTO) (*dto.SignInReply, error) {
@@ -29,6 +31,7 @@ func (s authService) SignIn(shop dto.SignInDTO) (*dto.SignInReply, error) {
 		}
 		//Save Shop to database.
 		if err := s.shopRepo.Create(models.Shop{
+			Model:      gorm.Model{ID: shop.ID},
 			Name:       shop.Name,
 			Email:      shop.Email,
 			FacebookID: shop.ID,
@@ -59,6 +62,27 @@ func (s authService) SignIn(shop dto.SignInDTO) (*dto.SignInReply, error) {
 			//TODO : handler error.
 			logs.Error(err)
 			return nil, err
+		}
+
+		//Save page repo.
+		//Check page repo.
+		for _, pageInfo := range res.Data {
+			id, err := strconv.Atoi(pageInfo.ID)
+			if err != nil {
+				//TODO : Handler error.
+				return nil, err
+			}
+			if _, err := s.pageRepo.CreaetOrFind(models.PageFacebook{
+				Model:       gorm.Model{ID: uint(id)},
+				AccessToken: pageInfo.AccessToken,
+				Name:        pageInfo.Name,
+				Picture:     pageInfo.Picture.Data.Url,
+				ShopID:      shop.ID,
+			}); err != nil {
+				//TODO : Handler error.
+				return nil, err
+			}
+
 		}
 		return &res, nil
 	}
